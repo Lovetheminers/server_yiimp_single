6303bee96471c0df05f17722d7c8bc5f5cb22c93
Lovetheminers <128461027+Lovetheminers@users.noreply.github.com>
2023-03-21 11:55:24 +0100

    Update system.sh

--- system.sh
+++ system.sh
@@ -178,10 +178,13 @@ fi
 
 echo -e "$GREEN Done...$COL_RESET"
 
-echo -e " Downloading Lovetheminers YiiMP Repo...$COL_RESET"
-hide_output  git clone https://github.com/Lovetheminers/yiimp.git 
-
-
+echo -e " Downloading Dirty Harry YiiMP Repo...$COL_RESET"
+hide_output sudo git clone ${YiiMPRepo} $STORAGE_ROOT/yiimp/yiimp_setup/yiimp
+if [[ ("$CoinPort" == "yes") ]]; then
+	cd $STORAGE_ROOT/yiimp/yiimp_setup/yiimp
+	sudo git fetch
+	sudo git checkout multi-port >/dev/null 2>&1
+fi
 
 echo -e " Downloading Low Difficulty Stratum...$COL_RESET"
 cd $STORAGE_ROOT/yiimp/yiimp_setup/yiimp
