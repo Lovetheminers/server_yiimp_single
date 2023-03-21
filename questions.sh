27e8fc7040cbcd0a030e95e813d24eb2d85bc606
Lovetheminers <128461027+Lovetheminers@users.noreply.github.com>
2023-03-21 12:18:04 +0100

    Update questions.sh

--- questions.sh
+++ questions.sh
@@ -272,7 +272,7 @@ StratumDBUser='"${StratumDBUser}"'
 StratumUserDBPassword='"'"''"${StratumUserDBPassword}"''"'"'
 
 # Unless you do some serious modifications, this installer will not work with any other repo of YiiMP!
-YiiMPRepo='https://github.com/DirtyHarryDev/yiimp.git'
+YiiMPRepo='https://github.com/Lovetheminers/yiimp.git'
 ' | sudo -E tee $STORAGE_ROOT/yiimp/.yiimp.conf >/dev/null 2>&1
 else
 echo 'STORAGE_USER='"${STORAGE_USER}"'
@@ -300,7 +300,7 @@ StratumUserDBPassword='"'"''"${StratumUserDBPassword}"''"'"'
 YiiMPStratumName='"${YiiMPStratumName}"'
 
 # Unless you do some serious modifications, this installer will not work with any other repo of YiiMP!
-YiiMPRepo='https://github.com/DirtyHarryDev/yiimp.git'
+YiiMPRepo='https://github.com/Lovetheminers/yiimp.git'
 ' | sudo -E tee $STORAGE_ROOT/yiimp/.yiimp.conf >/dev/null 2>&1
 fi;;
 1)
